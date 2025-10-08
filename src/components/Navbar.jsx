3ac6c28eb67a3388ff867c4f0a56a8390996e93e
Bala <malharikingdom9@gmail.com>
2025-10-08 19:35:40 +0530

    image Correction - 08/10-01

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Menu, X } from 'lucide-react'
 import { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import Logo from '/TPP.jpeg'
+import Logo from '../../Public/TPP.jpeg'
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
